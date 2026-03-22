3637c77b0561b6bb8df236441a5ec3ea0d7761ab
pepeforg1234 <pty.11111@gmail.com>
2026-03-22 21:39:47 +0800

    Initial Commit

---
+++ FreeDV/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FreeDV
+//
+//  Created by 李後陞 on 2026/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FreeDV/FreeDVApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreeDVApp.swift
+//  FreeDV
+//
+//  Created by 李後陞 on 2026/3/22.
+//
+
+import SwiftUI
+
+@main
+struct FreeDVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
